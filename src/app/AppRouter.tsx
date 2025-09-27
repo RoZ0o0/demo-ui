f0f36f26bd63a7f9807fbc38623806d00c61c850
RoZ0o0 <62017852+RoZ0o0@users.noreply.github.com>
2025-09-27 14:16:29 +0200

    chore: convert project to TypeScript

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import MainLayout from '../layouts/MainLayout';
+import MainLayout from "../layouts/Mainlayout";
 import Home from "../pages/Home";
 
 const AppRouter = () => {
